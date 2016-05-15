ec2d8071f6289a1821dd9b544493258b1cbf6ef1
Fabien Arrault <farrault@ippon.fr>
2016-05-15 13:47:25 +0200

    With Lambda

--- src/main/java/me/pattern/Main.java
+++ src/main/java/me/pattern/Main.java
@@ -27,36 +27,44 @@ public class Main {
     static <T, R> MatchCase<T, R> match() {
         return new MatchCase<>();
     }
-
-
+    
     static class MatchCase<T, R> implements Function<T, R> {
 
-        final List<Case> cases;
+        final List<Case<T, R>> cases;
 
         MatchCase() {
             this(Collections.emptyList());
         }
 
-        MatchCase(List<Case> cases) {
+        MatchCase(List<Case<T, R>> cases) {
             this.cases = cases;
         }
 
-
-        When when(Predicate<? super T> predicate) {
-            return new When(predicate, cases);
+        When<T, R> when(Predicate<? super T> predicate) {
+            return function -> addCase(predicate, function);
         }
+        
+    	private MatchCase<T, R> addCase(Predicate<? super T> predicate, Function<? super T, ? extends R> function) {
+    		Case<T, R> newCase = new Case<T, R>(predicate, function);
+    		
+    		List<Case<T, R>> cases = new ArrayList<>();
+    		cases.addAll(this.cases); // // Attention : capture du contexte => "leak" mémoire => ou pas ... on peut chainer ...
+			cases.add(newCase);
+    		
+    		return new MatchCase<T, R>(cases); // parcequ'on est immutable à la base là... mais bon ...
+    	}
 
-        When whenIs(Object pattern) {
+        When<T, R> whenIs(Object pattern) {
             return when(pattern::equals);
         }
 
-        When whenTypeIs(Class<?> cls) {
+        When<T, R> whenTypeIs(Class<?> cls) {
             return when(v -> cls.isAssignableFrom(v.getClass()));
         }
 
-        Otherwise otherwise(Function<? super T, ? extends R> function) {
-        	MatchCase<T, R> then = when(value -> true).then(function);
-            return new Otherwise(then);
+        Otherwise<T, R> otherwise(Function<? super T, ? extends R> function) {
+        	MatchCase<T, R> then = addCase(value -> true, function);
+            return (Otherwise<T, R>) ( then::apply );
         }
 
 
@@ -69,69 +77,36 @@ public class Main {
                     .orElseThrow(IllegalArgumentException::new);
         }
 
+    }
+    
+    interface When<T,R> {
+    	MatchCase<T, R> then(Function<? super T, ? extends R> function);
+    }
 
-        class When {
-
-            final Predicate<? super T> predicate;
-            final List<Case> cases;
-
-            When(Predicate<? super T> predicate, List<Case> cases) {
-                this.predicate = predicate;
-                this.cases = cases;
-            }
-
-            MatchCase<T, R> then(Function<? super T, ? extends R> function) {
-                List<Case> cases = new ArrayList<>();
-                cases.addAll(this.cases);
-                cases.add(new Case(When.this.predicate, function));
-
-                return new MatchCase<>(cases);
-            }
-
-        }
-
-//        An interface (which is like a static class), must be parameterized with <T,R> : 
-//        interface Otherwise<T,R> extends Function<T, R> {
-//        	
-//        };
-        
-        class Otherwise implements Function<T, R> {
-        	
-        	final MatchCase<T,R> matchCaseWithOtherwise;
-        	
-        	Otherwise(MatchCase<T,R> matchCaseWithOtherwise) {
-        		this.matchCaseWithOtherwise = matchCaseWithOtherwise;
-        	}
-        	
-        	@Override
-        	public R apply(T value) {
-        		return matchCaseWithOtherwise.apply(value);
-        	}
-        	
-        }
-
-        class Case implements Function<Object, R> {
-
-            final Predicate<Object> predicate;
-            final Function<Object, ? extends R> function;
-
-            @SuppressWarnings("unchecked")
-            Case(Predicate<? super T> predicate, Function<? super T, ? extends R> function) {
-                this.predicate = (Predicate<Object>) predicate;
-                this.function = (Function<Object, ? extends R>) function;
-            }
-
-            boolean isApplicable(Object object) {
-                return predicate.test(object);
-            }
-
-            @Override
-            public R apply(Object object) {
-                return function.apply(object);
-            }
-
-        }
-
+    interface Otherwise<T,R> extends Function<T, R> {
+    	
+    };
+    
+    static class Case<T, R> { // implements Function<Object, R> {
+    	
+    	final Predicate<Object> predicate;
+    	final Function<Object, ? extends R> function;
+    	
+    	@SuppressWarnings("unchecked")
+    	Case(Predicate<? super T> predicate, Function<? super T, ? extends R> function) {
+    		this.predicate = (Predicate<Object>) predicate;
+    		this.function = (Function<Object, ? extends R>) function;
+    	}
+    	
+    	boolean isApplicable(Object object) {
+    		return predicate.test(object);
+    	}
+    	
+//    	@Override
+    	public R apply(Object object) {
+    		return function.apply(object);
+    	}
+    	
     }
 
 }
