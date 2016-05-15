bb9b8fc8af1650ed8ead29590a06a8d514dd4b5b
Fabien Arrault <farrault@ippon.fr>
2016-05-15 14:47:55 +0200

    With type safety

--- src/main/java/me/pattern/Main.java
+++ src/main/java/me/pattern/Main.java
@@ -11,65 +11,82 @@ import java.util.function.Predicate;
 public class Main {
 
     public static void main(String[] args) {
-        List<?> values = Arrays.asList("hello", 1, 1.5);
+        List<?> values = Arrays.asList(new Child(), "hello", 1, 1.5);
 
         values.stream()
                 .map(
                         match()
                                 .whenIs("hello").then(__ -> "hello world")
                                 .whenTypeIs(Integer.class).then(i -> "int: " + i)
+                                .whenTypeIs(Root.class).then(r -> "name : " + r.getName())
                                 .otherwise(__ -> "what else!")
                 )
                 .forEach(System.out::println);
     }
 
+    static abstract class Root {
+    	abstract String getName();
+    }
+    
+    static class Child extends Root {
 
-    static <T, R> MatchCase<T, R> match() {
+		@Override
+		String getName() {
+			return "Child";
+		}
+    	
+    }
+    
+
+    static <R> MatchCase<R> match() {
         return new MatchCase<>();
     }
     
-    static class MatchCase<T, R> implements Function<T, R> {
+    static class MatchCase<R> implements Function<Object, R> {
 
-        final List<Case<T, R>> cases;
+        final List<Case<?,R>> cases;
 
         MatchCase() {
             this(Collections.emptyList());
         }
 
-        MatchCase(List<Case<T, R>> cases) {
+        MatchCase(List<Case<?,R>> cases) {
             this.cases = cases;
         }
 
-        When<T, R> when(Predicate<? super T> predicate) {
-            return function -> addCase(predicate, function);
+        <T> When<T,R> when(Predicate<Object> predicate) {
+        	return function -> addCase(predicate,function);
         }
         
-    	private MatchCase<T, R> addCase(Predicate<? super T> predicate, Function<? super T, ? extends R> function) {
-    		Case<T, R> newCase = new Case<T, R>(predicate, function);
+    	private <T> MatchCase<R> addCase(Predicate<Object> predicate, Function<T, ? extends R> function) {
+    		Case<T,R> newCase = new Case<T,R>(predicate, function);
     		
-    		List<Case<T, R>> cases = new ArrayList<>();
+    		List<Case<?,R>> cases = new ArrayList<>();
     		cases.addAll(this.cases); // // Attention : capture du contexte => "leak" mémoire => ou pas ... on peut chainer ...
 			cases.add(newCase);
     		
-    		return new MatchCase<T, R>(cases); // parcequ'on est immutable à la base là... mais bon ...
+    		return new MatchCase<R>(cases); // parcequ'on est immutable à la base là... mais bon ...
     	}
 
-        When<T, R> whenIs(Object pattern) {
+        <T> When<T,R> whenIs(T pattern) {
             return when(pattern::equals);
         }
 
-        When<T, R> whenTypeIs(Class<?> cls) {
-            return when(v -> cls.isAssignableFrom(v.getClass()));
+        <T> When<T,R> whenTypeIs(Class<T> cls) {
+            return when(v -> {
+        		System.out.println("---->"+v);
+        		return cls.isAssignableFrom(v.getClass()); // isAssignableFrom est-il encore nécessaire ? !! OUI carrément !
+            });
         }
 
-        Otherwise<T, R> otherwise(Function<? super T, ? extends R> function) {
-        	MatchCase<T, R> then = addCase(value -> true, function);
-            return (Otherwise<T, R>) ( then::apply );
+        Otherwise<R> otherwise(Function<Object, ? extends R> function) {
+        	MatchCase<R> then = addCase(value -> true, function);
+            return (Otherwise<R>) ( then::apply );
         }
 
 
         @Override
-        public R apply(T value) {
+        public R apply(Object value) {
             return cases.stream()
                     .filter(c -> c.isApplicable(value))
                     .findFirst()
@@ -80,31 +97,35 @@ public class Main {
     }
     
     interface When<T,R> {
-    	MatchCase<T, R> then(Function<? super T, ? extends R> function);
+    	MatchCase<R> then(Function<T, ? extends R> function);
     }
 
-    interface Otherwise<T,R> extends Function<T, R> {
+    interface Otherwise<R> extends Function<Object,R> {
     	
     };
     
-    static class Case<T, R> { // implements Function<Object, R> {
+    static class Case<T,R> { // implements Function<ObjecR> {
     	
-    	final Predicate<Object> predicate;
-    	final Function<Object, ? extends R> function;
+    	final Predicate<Object> predicate; // TODO : passer le prédicate en T ?
+    	final Function<T, ? extends R> function;
     	
     	@SuppressWarnings("unchecked")
-    	Case(Predicate<? super T> predicate, Function<? super T, ? extends R> function) {
-    		this.predicate = (Predicate<Object>) predicate;
-    		this.function = (Function<Object, ? extends R>) function;
+    	Case(Predicate<Object> predicate, Function<T, ? extends R> function) {
+    		this.predicate = predicate;
+    		this.function = function;
     	}
     	
     	boolean isApplicable(Object object) {
-    		return predicate.test(object);
+    		System.out.println("->"+object);
+    		// TODO : tester que c'est bien un T ?
+    		return predicate.test((Object)object);   // Comment ca peut fonctionner ??? => C'est à cause du type erasure ... au runtime c'est un (Object)
     	}
     	
 //    	@Override
-    	public R apply(Object object) {
-    		return function.apply(object);
+//    	/*public*/ R apply(T object) {
+    	/*public*/ R apply(Object object) { // peut pas être T sinon impossible de faire le cast au dessus
+//    		return function.apply((T)object);
+    		return function.apply((T)object);
     	}
     	
     }
